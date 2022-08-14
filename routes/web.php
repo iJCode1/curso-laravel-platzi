f06b4e55ff3cd6ac1d7c97002dec0e67b3548360
Joel <joeldome17@gmail.com>
2022-08-13 23:25:10 -0500

    feat: rutas en laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/**
+ * Route::get    | Consultar
+ * Route::post   | Guardar
+ * Route::delete | Eliminar
+ * Route::put    | Actualizar
+ */
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/buscar', function(){
+    return "Haciendo una busqueda";
+});
+// http://localhost:8000/buscar
+
+Route::get('/buscar/{param}', function($param){
+    return $param;
+});
+// http://localhost:8000/buscar/heeey
+
+Route::get('/buscar2', function(Request $request){
+    return $request->all();
+});
+// http://localhost:8000/buscar2/?query=algo
