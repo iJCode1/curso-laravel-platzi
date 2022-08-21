57af50e0659148efec2ec7e495821ff88e55fc87
Joel <joeldome17@gmail.com>
2022-08-20 21:19:47 -0500

    feat: Listado de publicaciones

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $primaryKey = 'id';
+    protected $fillable = ['id', 'title', 'slug', 'user_id'];
 
     public function user(){
       return $this->belongsTo(User::class);
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,6 +4,33 @@
 
 @section('content')
   <div class="container">
-    <h2 class="text-primary mb-4">Listado de publicaciones</h2>
+    <h2 class="display-4 mb-4">Listado de publicaciones</h2>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Post</th>
+          <th scope="col">Editar</th>
+          <th scope="col">Eliminar</th>
+        </tr>
+      </thead>
+      <tbody>
+        @forelse($posts as $post)   
+          <tr>
+            <th scope="row">{{ $post->id }}</th>
+            <td>{{ $post->title }}</td>
+            <td>
+              <a href="#">Editar</a>
+            </td>
+            <td>Eliminar</td>
+          </tr>
+          @empty
+          <tr class="">
+            Upps! no hay ninguna publicacion disponible
+          </tr>
+        @endforelse
+      </tbody>
+    </table>
+    {{ $posts->links() }}
   </div>
 @endsection
