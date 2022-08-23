c7d1877323956b10a3134cb88ee6c868f0933a65
Joel <joeldome17@gmail.com>
2022-08-23 00:15:53 -0500

    feat: Diseño de template e inicio de vista home

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,21 +5,40 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>iJCode - @yield('title')</title>
+  
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-  
-  <nav class="nav">
-    <ul class="nav-list">
-      <li class="nav-element">
-        <a href="{{ route('home') }}" class="nav-link">Home</a>
-      </li>
-      <li class="nav-element">
-        <a href="{{ route('blog') }}" class="nav-link">Blog</a>
-      </li>
-    </ul>
-  </nav>
+  <div class="container mx-auto">
+    <nav class="navbar d-flex justify-between align-items-center">
+      <ul class="navbar-nav d-flex flex-row align-items-center">
+        <li class="nav-item">
+          <a href="{{route('home')}}" class="nav-link">
+            <img src="{{asset('images/logo.png')}}" alt="Ir al Home" width="60">
+          </a>
+        </li>
+        <li class="nav-item ml-4">
+          <input class="form-control" type="search" placeholder="Buscar"/>
+        </li>
+      </ul>
+      <ul class="navbar-nav">
+        @guest
+        <li class="nav-item">
+            <a class="nav-link text-dark text-uppercase" href="{{ route('login') }}">{{ __('Login') }}</a>
+        </li>
+        @endguest
+      </ul>
+    </nav>
+    <p style="height: 2px; background: linear-gradient(to right, 
+    rgba(200, 200, 200, 0) 0%,
+    rgba(200, 200, 200, 1) 30%,
+    rgba(200, 200, 200, 1) 70%,
+    rgba(200, 200, 200, 0) 100%
+    );"></p>
 
-  @yield('content')
+    @yield('content')
 
+  </div>
 </body>
 </html>
